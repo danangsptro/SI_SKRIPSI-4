9e3e33dd6130fd32054f2da4f5d896cfdccc4e5c
Asip Hamdi <asiphamdi@gmail.com>
2022-07-31 14:15:03 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,61 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
+<html lang="en">
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <!-- Title -->
+    <!-- Title --> 
     <link rel="icon" href="{{ asset('images/logo/tangsel.png') }}" type="image/x-icon">
     <title>{{ env('APP_NAME') }} | Form Login</title>
 
-    <!-- CSS -->
-    <link href="{{ asset('assets/css/sb-admin-2.min.css') }}" rel="stylesheet">
-
     <!-- Icon -->
     <link href="{{ asset('assets/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet"> 
 
-    <!-- Animate CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <!-- CSS -->
+    <link href="{{ asset('assets/css/sb-admin-2.min.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/util.css') }}">
 
 </head>
-
-<body class="bg-gradient-primary">
-    <div class="col-md-4 container">
-        <div class="">
-            <div class="card o-hidden border-0 shadow-lg my-5">
-                <div class="card-body p-0">
-                    <div class="">
-                        <div class="p-5">
-                            <div class="text-center">
-                                <h1 class="h4 text-gray-900 mb-4">Login</h1>
-                            </div>
-                            <form class="needs-validation user" novalidate method="POST" action="{{ route('login') }}" autocomplete="off">
-                                @csrf
-                                <div class="form-group">
-                                    <input type="email" class="form-control form-control-user @if ($errors->has('email')) is-invalid @endif" name="email" id="email" placeholder="Masukan E-mail" autofocus>
-                                    @if ($errors->has('email'))
-                                    <div class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </div>
-                                    @endif
-                                </div>
-                                <div class="form-group">
-                                    <input type="password" class="form-control form-control-user  @if ($errors->has('passowrd')) is-invalid @endif" name="passowrd" id="password" placeholder="Password">
-                                    @if ($errors->has('password'))
-                                    <div class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </div>
-                                    @endif
-                                </div>
-                                <div class="form-group">
-                                    <div class="custom-control custom-checkbox small">
-                                        <input type="checkbox" class="custom-control-input" id="customCheck">
-                                        <label class="custom-control-label" for="customCheck">Remember Me</label>
-                                    </div>
-                                </div>
-                                <button type="submit" class="btn btn-primary btn-user btn-block font-weight-bold"><i class="fa fa-arrow-right mr-2"></i>Login</button>
-                            </form>
-                        </div>
+<body class="bg-gradient-light">
+    <div class="container col-md-4">
+        <div class="card o-hidden border-0 shadow-lg my-5">
+            <div class="card-body p-0">
+                <div class="p-5">
+                    <div class="text-center">
+                        <h1 class="h4 text-gray-900 mb-4 font-weight-bold">Login</h1>
                     </div>
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+                        <div class="form-group">
+                            <input type="email" class="form-control fs-14 @error('email') is-invalid @enderror" id="email" name="email" placeholder="Masukan Email" autofocus>
+                            @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control fs-14 @error('password') is-invalid @enderror" id="password" name="password" placeholder="Masukan Password">
+                            @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                        <button class="btn btn-primary btn-block"><i class="fa fa-arrow-right mr-2"></i>Login</button>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
 </body>
+    <!-- Script -->
+    <script src="{{ asset('assets/vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+    <script src="{{ asset('assets/js/sb-admin-2.min.js') }}"></script>
 </html>
